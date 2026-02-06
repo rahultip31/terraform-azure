4b6ba3e3ab722f3ca2b1e128fd3f3af2a3671118
rahultip31 <rahultip31@gmail.com>
2026-02-06 20:59:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,12 +22,12 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = "demo-vnet"
-#   address_space       = ["10.0.0.0/16"]
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-# }
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demo-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
 # resource "azurerm_subnet" "subnet1" {
 #   name                 = "subnet-1"
