feee50f2ce5f706a3e90d8e0c96e151626af5b63
rahultip31 <rahultip31@gmail.com>
2026-02-06 20:12:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,3 +35,10 @@ resource "azurerm_subnet" "subnet1" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet-2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+} 
