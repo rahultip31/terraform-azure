c2d2d5502cf77620ab2e74101ea185ff6a886d2a
rahultip31 <rahultip31@gmail.com>
2026-02-06 20:20:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,23 +22,16 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "demo-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = "demo-vnet"
+#   address_space       = ["10.0.0.0/16"]
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
 
-resource "azurerm_subnet" "subnet1" {
-  name                 = "subnet-1"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_subnet" "subnet2" {
-  name                 = "subnet-2"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-} 
+# resource "azurerm_subnet" "subnet1" {
+#   name                 = "subnet-1"
+#   resource_group_name  = azurerm_resource_group.rg.name
+#   virtual_network_name = azurerm_virtual_network.vnet.name
+#   address_prefixes     = ["10.0.1.0/24"]
+# }
